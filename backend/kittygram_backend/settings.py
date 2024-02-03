ce1c9d23f6f2b7b267c2a24f2a74666ddc130c1d
Yuliy Pavlov <yuliypavlov@gmail.com>
2024-02-03 20:10:22 +0500

    Add python-dotenv

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,15 +1,11 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='default')
 
-DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
